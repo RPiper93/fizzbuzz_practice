c1fc5a31473c0f883ffe1e96e70d3b68d62fdae5
Rebecca Piper <piper.rebecca93@gmail.com>
2016-01-13 12:03:20 +0000

    Updated spec file with more tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,16 +10,26 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(1)).to eq 1
   end
 
-  [15, 30, 45].each do |x|
+  (1..100).each do |x|
     it 'returns "fizzbuzz" when number is a product of 3 and 5' do
-      expect(fizzbuzz(x)).to eq 'fizzbuzz'
+      if x % 15 == 0
+        expect(fizzbuzz(x)).to eq 'fizzbuzz'
+      end
     end
   end
 
-  (1..50).each do |y|
+  (1..100).each do |y|
     it 'returns "buzz" when number is divisible by 5 and not by 3' do
       if y % 5 == 0 && y % 3 != 0
         expect(fizzbuzz(y)).to eq 'buzz'
+      end
+    end
+  end
+
+  (1..100).each do |y|
+    it 'returns "fizz" when number is divisible by 3 and not by 5' do
+      if y % 3 == 0 && y % 5 != 0
+        expect(fizzbuzz(y)).to eq 'fizz'
       end
     end
   end
