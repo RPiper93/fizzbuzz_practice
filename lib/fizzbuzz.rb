cbb25ae1d403cc8b6915145eac04b55a1c2dde45
Rebecca Piper <piper.rebecca93@gmail.com>
2016-01-13 11:38:10 +0000

    First Test Passed

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,5 @@
+def fizzbuzz(number)
+
+  return 1 if number == 1
+  'fizz'
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,13 @@
+
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns 1 when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+
+end
