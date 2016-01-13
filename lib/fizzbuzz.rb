fa53e9d7a4070b6657b5bac8400ac2b605bef4b9
Rebecca Piper <piper.rebecca93@gmail.com>
2016-01-13 11:45:45 +0000

    Second and Third Test Passed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,6 @@
 def fizzbuzz(number)
 
   return 1 if number == 1
+  return 'fizzbuzz' if number % 15 == 0
   'fizz'
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,4 +10,10 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(1)).to eq 1
   end
 
+  [15, 30, 45].each do |x|
+    it 'returns "fizzbuzz" when number is a product of 3 and 5' do
+      expect(fizzbuzz(x)).to eq 'fizzbuzz'
+    end
+  end
+
 end
